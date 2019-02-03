21694820e26d11a5ba7790c0dd877f931f17b8b9
Liz <erbanach@gmail.com>
2019-02-02 20:41:18 -0800

    base setup/styles for landing page

--- source/app.js
+++ source/app.js
@@ -1,24 +1,7 @@
-import bus from '/es6/helpers/bus.js'
-import keyboard from '/es6/helpers/keyboard.js'
+import $ from 'jquery'
 
-import scroll from '/es6/components/scroll.js'
-import nav from '/es6/components/nav.js'
+import fadeIn from '/es6/components/fade-in.js'
 
-const init = () => {
-  // keyboard()
-  // scroll()
-  // nav()
-}
-
-var app = {
-  channel: 'app:init',
-  description: 'Initializes application',
-  emits: 'N/A',
-  listeners: []
-}
-
-bus.register(app)
-bus.on('app:init', init)
-bus.emit('app:init')
-
-console.log(bus.channels)
+$(document).ready(() => {
+  fadeIn()
+})
---
+++ source/es6/components/fade-in.js
@@ -0,0 +1,7 @@
+import $ from 'jquery'
+
+const fadeIn = () => {
+  $(document.body).fadeIn(1200)
+}
+
+export default fadeIn
